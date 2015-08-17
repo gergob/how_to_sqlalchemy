5e10d88b5869e6ba3023dc7a5631497675aead3c
Gergo Bogdan <gergo.bogdan@accedo.tv>
2015-08-17 21:27:37 +0200

    added base logic for app

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 __author__ = 'Gergo'
 
+from DAL import DAL
+import init_database as Init
 
-def run_app():
-    pass
+engine = 'mysql://johndoe:secret@localhost/how_to_sqlalchemy'
+my_dal = None
 
+def run_app(should_run_init):
+    if should_run_init:
+        Init.init(engine)
+    else:
+        my_dal = DAL(engine)
 
 if __name__ == '__main__':
-    run_app()
+    run_app(True)
